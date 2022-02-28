642993de0c7aafc833401fc0af921524ed8cfab3
semen.trofimov <semen.trofimov@coolclever.ru>
2022-02-28 12:55:55 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express, { json } from "express";
+import swaggerUi from "swagger-ui-express";
+
+import router from "./src/routes/index.route.js";
+import config from "./src/config/config.js";
+import { swaggerDocument } from "./src/swagger/swagger.js";
+
+const { PORT } = config;
+
+const app = express();
+
+app.use(json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`start on ${PORT}`);
+});
