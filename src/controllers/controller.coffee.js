eff857251cade979e6682d51275c017e1ca7a886
Димов Денис Андреевич <Dimov_DA@cd.local>
2022-03-11 09:31:03 +0300

    fix card_id

--- src/controllers/controller.coffee.js
+++ src/controllers/controller.coffee.js
@@ -3,33 +3,31 @@ import { Validator } from "jsonschema";
 import execute from "../config/db.js";
 import schema from "../config/schema.js";
 
-// const data = {
-//   product_id: 123,
-//   card_id: "vncvncvhjbsfdfgh34",
-//   device_id: "coffee",
-// };
-
 export async function coffee(req, res) {
   try {
+    let textsql;
+    let result;
     const { product_id, card_id, device_id } = req.body;
     const data = req.body;
     const v = new Validator();
     const validate = v.validate(data, schema);
-    let textsql;
-    let result;
+
     if (validate.errors.length > 0) {
       throw new Error(validate.errors);
     } else {
       textsql = `
-        INSERT INTO [coffee].[dbo].[coffee]
-        ([product_id],[card_id],[device_id])
-        VALUES (
-        ${data.product_id}, '${data.card_id}', '${data.device_id}'
-      )`;
-
+      SELECT
+        [card_id]
+    FROM [coffee].[dbo].[coffee]
+    `;
       result = await execute(textsql);
-    }
 
+      const res = result.find((i) => i.card_id === card_id);
+      if (res) {
+      } else {
+        throw new Error("error");
+      }
+    }
     res.status(200).json({ result: "success" });
   } catch (err) {
     res.status(403).json({
@@ -52,6 +50,7 @@ export async function coffee(req, res) {
 //     `;
 
 //     result = await execute(textsql);
+//     console.log(result);
 
 //     const arr = result.map((item) => {
 //       const obj = {
@@ -60,24 +59,6 @@ export async function coffee(req, res) {
 //       return obj;
 //     });
 
-//     let amount = 0;
-
-//     if (amount != 0) {
-//       for (let i = 0; i < arr.length; i++) {
-//         const obj = arr[i];
-
-//         textsql = `
-//         INSERT INTO [coffee].[dbo].[person]
-//           ([card_id],[amount],[dt])
-//         VALUES (
-//         '${obj.id}', ${amount}, convert(varchar(20),getdate(),120)
-//       )`;
-//         result = await execute(textsql);
-//       }
-//       res.status(200).json("доступно");
-//     } else {
-//       throw new Error("ноль");
-//     }
 //   } catch (err) {
 //     res.status(403).json({ error: err.message });
 //   }
